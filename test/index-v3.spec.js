73b11577e05ae68a961b17588f47e17a57513f89
Fattafatta <39348268+Fattafatta@users.noreply.github.com>
2023-03-10 12:46:38 +0100

    feat: add automatic jsx version detection
    The plugin now automatically detects the version of jsx used and runs the required transformations.
    It is no longer necessary to specify a version in options.

--- test/index-v3.spec.js
+++ test/index-v3.spec.js
@@ -1,54 +1,45 @@
-import chai, { expect } from "chai";
-import convertTo from "./chai-convert-to";
-
-chai.use(
-  convertTo(
-    "convertToV3",
-    { plugins: [[require.resolve("../"), { jsx: "v3" }],"@babel/syntax-object-rest-spread"] },
-    { plugins: ["@babel/syntax-jsx", "@babel/syntax-object-rest-spread"] }
-  )
-);
+import { expect } from "chai";
 
 describe("createElement-to-JSX", () => {
   it("should support all ReScript function calls", () => {
-    expect('React.createElement("h1")').to.convertToV3("<h1 />;");
-    expect('ReactDOM.createElement("h1")').to.convertToV3("<h1 />;");
-    expect('ReactDOMRe.createElement("h1")').to.convertToV3("<h1 />;");
-    expect('ReactDOMRe.createDOMElementVariadic("h1")').to.convertToV3("<h1 />;");
-    expect('ReactDOM.createDOMElementVariadic("h1")').to.convertToV3("<h1 />;");
-    expect('React.createElementVariadic("h1")').to.convertToV3("<h1 />;");
+    expect('React.createElement("h1")').to.convertTo("<h1 />;");
+    expect('ReactDOM.createElement("h1")').to.convertTo("<h1 />;");
+    expect('ReactDOMRe.createElement("h1")').to.convertTo("<h1 />;");
+    expect('ReactDOMRe.createDOMElementVariadic("h1")').to.convertTo("<h1 />;");
+    expect('ReactDOM.createDOMElementVariadic("h1")').to.convertTo("<h1 />;");
+    expect('React.createElementVariadic("h1")').to.convertTo("<h1 />;");
   });
   it("should convert 1-argument calls", () => {
-    expect('React.createElement("h1")').to.convertToV3("<h1 />;");
-    expect('ReactDOMRe.createDOMElementVariadic("h1")').to.convertToV3("<h1 />;");
-    expect("React.createElement(Foo)").to.convertToV3("<Foo />;");
-    expect("React.createElement(Foo.Bar)").to.convertToV3("<Foo.Bar />;");
-    expect("React.createElement(Foo.Bar.Baz)").to.convertToV3("<Foo.Bar.Baz />;");
+    expect('React.createElement("h1")').to.convertTo("<h1 />;");
+    expect('ReactDOMRe.createDOMElementVariadic("h1")').to.convertTo("<h1 />;");
+    expect("React.createElement(Foo)").to.convertTo("<Foo />;");
+    expect("React.createElement(Foo.Bar)").to.convertTo("<Foo.Bar />;");
+    expect("React.createElement(Foo.Bar.Baz)").to.convertTo("<Foo.Bar.Baz />;");
   });
 
   it("should convert effective 1-argument calls (with null or undefined)", () => {
-    expect('React.createElement("h1", null)').to.convertToV3("<h1 />;");
-    expect('React.createElement("h2", null, null)').to.convertToV3("<h2 />;");
-    expect('React.createElement("h3", undefined)').to.convertToV3("<h3 />;");
+    expect('React.createElement("h1", null)').to.convertTo("<h1 />;");
+    expect('React.createElement("h2", null, null)').to.convertTo("<h2 />;");
+    expect('React.createElement("h3", undefined)').to.convertTo("<h3 />;");
   });
 
   it("should handle props without children", () => {
-    expect('React.createElement("h1", {hi: there})').to.convertToV3(
+    expect('React.createElement("h1", {hi: there})').to.convertTo(
       "<h1 hi={there} />;"
     );
-    expect('React.createElement("h2", {"hi": there})').to.convertToV3(
+    expect('React.createElement("h2", {"hi": there})').to.convertTo(
       "<h2 hi={there} />;"
     );
-    expect('React.createElement("h3", {hi: "there"})').to.convertToV3(
+    expect('React.createElement("h3", {hi: "there"})').to.convertTo(
       '<h3 hi="there" />;'
     );
   });
 
   it("should handle spread props", () => {
-    expect('React.createElement("h1", props)').to.convertToV3(
+    expect('React.createElement("h1", props)').to.convertTo(
       "<h1 {...props} />;"
     );
-    expect('React.createElement("h1", getProps())').to.convertToV3(
+    expect('React.createElement("h1", getProps())').to.convertTo(
       "<h1 {...getProps()} />;"
     );
   });
@@ -56,11 +47,11 @@ describe("createElement-to-JSX", () => {
   it("should handle mixed props", () => {
     expect(
       'React.createElement("h1", _extends({ hi: "there" }, props))'
-    ).to.convertToV3('<h1 hi="there" {...props} />;');
+    ).to.convertTo('<h1 hi="there" {...props} />;');
     expect(
       'React.createElement("h1", _extends({}, props, { hi: "there" }))'
-    ).to.convertToV3('<h1 {...props} hi="there" />;');
-    expect('React.createElement("h1", { ...props, hi: "there" })').to.convertToV3(
+    ).to.convertTo('<h1 {...props} hi="there" />;');
+    expect('React.createElement("h1", { ...props, hi: "there" })').to.convertTo(
       '<h1 {...props} hi="there" />;'
     );
   });
@@ -68,28 +59,28 @@ describe("createElement-to-JSX", () => {
   it("should handle props and ignore “null”/“undefined” children", () => {
     expect(
       'React.createElement("h1", {hi: there}, null, undefined)'
-    ).to.convertToV3("<h1 hi={there} />;");
+    ).to.convertTo("<h1 hi={there} />;");
   });
 
   it("should ignore “null”/“undefined” props and handle children", () => {
-    expect('React.createElement("h1", null, "Header")').to.convertToV3(
+    expect('React.createElement("h1", null, "Header")').to.convertTo(
       "<h1>Header</h1>;"
     );
     //this can be created from e.g. '<h2>Header{"harhar"}</h2>', but i think there’s no downside to merging it
-    expect('React.createElement("h2", null, "Header", "harhar")').to.convertToV3(
+    expect('React.createElement("h2", null, "Header", "harhar")').to.convertTo(
       "<h2>Headerharhar</h2>;"
     );
     expect(
       'React.createElement("h3", null, React.createElement("i"))'
-    ).to.convertToV3("<h3><i /></h3>;");
+    ).to.convertTo("<h3><i /></h3>;");
     expect(
       'React.createElement("h4", null, "a", React.createElement("b"), "c")'
-    ).to.convertToV3("<h4>a<b />c</h4>;");
+    ).to.convertTo("<h4>a<b />c</h4>;");
   });
 
   it("should handle props and children", () => {
     //we extensively tested props and children separately, so only sth. basic
-    expect('React.createElement("h1", {hi: there}, "Header")').to.convertToV3(
+    expect('React.createElement("h1", {hi: there}, "Header")').to.convertTo(
       "<h1 hi={there}>Header</h1>;"
     );
   });
@@ -97,12 +88,19 @@ describe("createElement-to-JSX", () => {
   it("should ignore intermingled “null”/“undefined” children", () => {
     expect(
       'React.createElement("h1", null, null, "Header", undefined)'
-    ).to.convertToV3("<h1>Header</h1>;");
+    ).to.convertTo("<h1>Header</h1>;");
   });
 
   it("should handle children in nested expressions", () => {
     expect(
       'React.createElement("h1", null, foo ? React.createElement("p") : null)'
-    ).to.convertToV3("<h1>{foo ? <p /> : null}</h1>;");
+    ).to.convertTo("<h1>{foo ? <p /> : null}</h1>;");
+  });
+
+  it("should handle react fragments", () => {
+    expect("React.createElement(React.Fragment, null)").to.convertTo("<></>;");
+    expect("React.createElement(ReasonReact.fragment, null)").to.convertTo("<></>;");
+    expect("React.createElement(JsxRuntime.Fragment, null)").to.convertTo("<></>;");
+    expect("React.createElement(JsxRuntime.jsxFragment, null)").to.convertTo("<></>;");
   });
 });
